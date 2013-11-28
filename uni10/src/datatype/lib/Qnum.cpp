4116a1bb5b8ed2c570fc68c0d9c824638ac90ae7
Tama Ma <pingnang@phys.ethz.ch>
2013-11-29 01:37:59 +0800

    follow-up

--- uni10/src/datatype/lib/Qnum.cpp
+++ uni10/src/datatype/lib/Qnum.cpp
@@ -1,4 +1,35 @@
-#include "Qnum.h"
+/*****************************************************************************
+*
+* Universal Tensor Network Library
+*
+* Copyright (C) 2013-2014 
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
+* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
+* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*
+* Please direct any enquiry to <development@uni10.org>
+*
+*****************************************************************************/
+
+#ifndef UNI10_DATATYPE_QNUM_CPP
+#define UNI10_DATATYPE_QNUM_CPP
+
+#include <uni10/datatype/Qnum.h>
+
+namespace uni10 {
+namespace datatype {
+
+const int Qnum::U1_UPB = 100;      // this should be made nicer...
+const int Qnum::U1_LOB = -100;      // ....
+const int Qnum::ptr_UPB = 2;
+const int Qnum::ptr_LOB = -1;
+
+
 bool operator< (const Qnum_t& q1, const Qnum_t& q2){
 	return (q1.U1 * 10 + q1.prt) < (q2.U1 * 10 + q2.prt);
 }
@@ -28,3 +59,8 @@ void Qnum_t::set(int _U1, int _prt){
 	U1 = _U1;	
 	prt = _prt;
 }
+
+} // ending namespace datatype
+} // ending namespace uni10
+
+#endif
