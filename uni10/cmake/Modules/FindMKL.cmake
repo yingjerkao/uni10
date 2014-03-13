8803a13b36a331f3062f351b790da7844a511bf5
Ying-Jer Kao <yjkao@phys.ntu.edu.tw>
2014-03-13 23:43:26 +0800

    New FindMKL.cmake module for finding MKL Modified top CMakeLists.txt accordingly

--- uni10/cmake/Modules/FindMKL.cmake
+++ uni10/cmake/Modules/FindMKL.cmake
@@ -1,48 +1,127 @@
-FIND_PATH(MKL_INCLUDE_DIR mkl_cblas.h /usr/include /usr/local/include $ENV{MKL_HOME}/include)
-IF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-  GET_FILENAME_COMPONENT(MKL_LIB_PATH ${MKL_INCLUDE_DIR}/../lib/64 ABSOLUTE)
-ELSE ( CMAKE_SYSTEM_PROCESSOR  STREQUAL "x86_64" )
-  GET_FILENAME_COMPONENT(MKL_LIB_PATH ${MKL_INCLUDE_DIR}/../lib/32 ABSOLUTE)
-ENDIF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-	
-FIND_LIBRARY( MKL_LAPACK NAMES mkl_lapack PATHS ${MKL_LIB_PATH} )
-IF ( MKL_LAPACK_FOUND )
-  # old MKL versions
-  IF ( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-     FIND_LIBRARY( MKL NAMES mkl_ia64 PATHS ${MKL_LIB_PATH} )
-  ELSE ( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-     FIND_LIBRARY( MKL NAMES mkl_ia32 PATHS ${MKL_LIB_PATH} )
-  ENDIF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-  FIND_LIBRARY( MKL_GUIDE NAMES guide PATHS ${MKL_LIB_PATH} )
-  SET (MKL ${MKL} ${MKL_LAPACK} ${MKL_GUIDE} )
-ELSE (MKL_LAPACK_FOUND )
-  # newer MKL version
-  SET (MKL_LAPACK "")
-  IF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-    GET_FILENAME_COMPONENT(MKL_LIB_PATH ${MKL_INCLUDE_DIR}/../lib/intel64 ABSOLUTE)
-  ELSE ( CMAKE_SYSTEM_PROCESSOR  STREQUAL "x86_64" )
-    GET_FILENAME_COMPONENT(MKL_LIB_PATH ${MKL_INCLUDE_DIR}/../lib/ia32 ABSOLUTE)
-  ENDIF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-	
-  FIND_LIBRARY( MKL_INTEL NAMES mkl_intel_lp64 PATHS ${MKL_LIB_PATH} )
-  FIND_LIBRARY( MKL_SEQUENTIAL NAMES mkl_sequential PATHS ${MKL_LIB_PATH} )
-  FIND_LIBRARY( MKL_CORE NAMES mkl_core PATHS ${MKL_LIB_PATH} )
-  SET (MKL ${MKL_INTEL} ${MKL_SEQUENTIAL} ${MKL_CORE} )
-ENDIF ( MKL_LAPACK_FOUND )
-	
-SET( MKL_BLAS_INCLUDE_FILE ${MKL_INCLUDE_DIR}/mkl_blas.h )
-SET( MKL_LAPACK_INCLUDE_FILE ${MKL_INCLUDE_DIR}/mkl_lapack.h )
+# - Find Intel MKL
+# Find the MKL libraries
+#
+# Options:
+#
+#   MKL_STATAIC       :   use static linking
+#   MKL_MULTI_THREADED:   use multi-threading
+#   MKL_SDL           :   Single Dynamic Library interface
+#
+# This module defines the following variables:
+#
+#   MKL_FOUND            : True if MKL_INCLUDE_DIR are found
+#   MKL_INCLUDE_DIR      : where to find mkl.h, etc.
+#   MKL_INCLUDE_DIRS     : set when MKL_INCLUDE_DIR found
+#   MKL_LIBRARIES        : the library to link against.
 
-IF (MKL_INCLUDE_DIR)
-  SET(MKL_FOUND ON)
-ENDIF (MKL_INCLUDE_DIR)
 
-IF (MKL_FOUND OR MKL_INTEL_FOUND )
-  IF (NOT MKL_FIND_QUIETLY)
-     MESSAGE(STATUS "Found MKL: ${MKL_INCLUDE_DIR}")
-  ENDIF (NOT MKL_FIND_QUIETLY)
-ELSE(MKL_FOUND OR MKL_INTEL_FOUND )
-  IF (MKL_FIND_REQUIRED)
-     MESSAGE(FATAL_ERROR "Could not find MKL")
-  ENDIF (MKL_FIND_REQUIRED)
-ENDIF (MKL_FOUND OR MKL_INTEL_FOUND )
+include(FindPackageHandleStandardArgs)
+
+set(INTEL_ROOT "/opt/intel" CACHE PATH "Folder contains intel libs")
+set(MKL_ROOT ${INTEL_ROOT}/mkl CACHE PATH "Folder contains MKL")
+
+# Find include dir
+find_path(MKL_INCLUDE_DIR mkl.h
+    PATHS ${MKL_ROOT}/include)
+
+# Find include directory
+if(WIN32)
+    find_path(INTEL_INCLUDE_DIR omp.h
+        PATHS ${INTEL_ROOT}/include)
+    set(MKL_INCLUDE_DIR ${MKL_INCLUDE_DIR} ${INTEL_INCLUDE_DIR})
+endif()
+
+# Find libraries
+if( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
+  find_path(MKL_LIB_PATH  libmkl_core.a PATHS ${MKL_ROOT}/lib/intel64)
+  message("architecture:x86_64 " ${MKL_LIB_PATH})
+else()
+  find_path(MKL_LIB_PATH  libmkl_core.a PATHS ${MKL_ROOT}/lib/ia32)
+endif()
+# Handle suffix
+set(_MKL_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+
+if(WIN32)
+    if(MKL_STATAIC)
+        set(CMAKE_FIND_LIBRARY_SUFFIXES .lib)
+    else()
+        set(CMAKE_FIND_LIBRARY_SUFFIXES _dll.lib)
+    endif()
+else()
+    if(MKL_STATAIC)
+        set(CMAKE_FIND_LIBRARY_SUFFIXES .a)
+    else()
+        set(CMAKE_FIND_LIBRARY_SUFFIXES .so)
+    endif()
+endif()
+
+
+# MKL is composed of four layers: Interface, Threading, Computational and RTL
+
+if(MKL_SDL)
+    find_library(MKL_LIBRARY mkl_rt PATHS ${MKL_LIB_PATH})
+    set(MKL_MINIMAL_LIBRARY ${MKL_LIBRARY})
+else()
+    ######################### Interface layer #######################
+    if(WIN32)
+        set(MKL_INTERFACE_LIBNAME mkl_intel_c)
+    else()
+      if( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
+        set(MKL_INTERFACE_LIBNAME mkl_intel_lp64)
+      else()
+        set(MKL_INTERFACE_LIBNAME mkl_intel)
+      endif()
+    endif()
+
+    find_library(MKL_INTERFACE_LIBRARY ${MKL_INTERFACE_LIBNAME}
+        PATHS ${MKL_LIB_PATH})
+
+    ######################## Threading layer ########################
+    if(MKL_MULTI_THREADED)
+        set(MKL_THREADING_LIBNAME mkl_intel_thread)
+    else()
+        set(MKL_THREADING_LIBNAME mkl_sequential)
+    endif()
+
+    find_library(MKL_THREADING_LIBRARY ${MKL_THREADING_LIBNAME}
+        PATHS ${MKL_LIB_PATH})
+
+    ####################### Computational layer #####################
+    find_library(MKL_CORE_LIBRARY mkl_core
+        PATHS ${MKL_LIB_PATH})
+    find_library(MKL_FFT_LIBRARY mkl_cdft_core
+        PATHS ${MKL_LIB_PATH})
+    if( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
+      find_library(MKL_SCALAPACK_LIBRARY mkl_scalapack_lp64
+            PATHS ${MKL_LIB_PATH})
+    else()
+      find_library(MKL_SCALAPACK_LIBRARY mkl_scalapack_core
+            PATHS ${MKL_LIB_PATH})
+    endif()
+
+    ############################ RTL layer ##########################
+    if(WIN32)
+        set(MKL_RTL_LIBNAME omp5md)
+    else()
+        set(MKL_RTL_LIBNAME iomp5)
+    endif()
+    if( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
+      find_library(MKL_RTL_LIBRARY ${MKL_RTL_LIBNAME}
+          PATHS ${INTEL_ROOT}/lib/intel64)
+    else()
+      find_library(MKL_RTL_LIBRARY ${MKL_RTL_LIBNAME}
+          PATHS ${INTEL_ROOT}/lib/ia32)
+    endif()
+
+    set(MKL_LIBRARY ${MKL_INTERFACE_LIBRARY} ${MKL_THREADING_LIBRARY} ${MKL_CORE_LIBRARY} ${MKL_FFT_LIBRARY} ${MKL_SCALAPACK_LIBRARY} ${MKL_RTL_LIBRARY})
+    set(MKL_MINIMAL_LIBRARY ${MKL_INTERFACE_LIBRARY} ${MKL_THREADING_LIBRARY} ${MKL_CORE_LIBRARY} ${MKL_RTL_LIBRARY})
+endif()
+set(CMAKE_FIND_LIBRARY_SUFFIXES ${_MKL_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
+
+find_package_handle_standard_args(MKL DEFAULT_MSG MKL_INCLUDE_DIR MKL_LIBRARY MKL_MINIMAL_LIBRARY)
+
+if(MKL_FOUND)
+    set(MKL_INCLUDE_DIRS ${MKL_INCLUDE_DIR})
+    set(MKL_LIBRARIES ${MKL_LIBRARY})
+    set(MKL_MINIMAL_LIBRARIES ${MKL_LIBRARY})
+endif()
