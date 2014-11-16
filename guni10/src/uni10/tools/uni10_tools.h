24c6c746b5ef3e5c8f29f97e58d75e1cfd0180dd
Yun-Da Hsieh <yundahsieh@gmail.com>
2014-11-16 22:03:38 +0800

    Merged

--- guni10/src/uni10/tools/uni10_tools.h
+++ guni10/src/uni10/tools/uni10_tools.h
@@ -37,11 +37,7 @@
 #include <functional>
 #include <cctype>
 #include <locale>
-<<<<<<< HEAD
 #include <sstream>
-=======
-#include <stdexcept>
->>>>>>> eb617fd2f94a713f341e55cde422fb32a424c0f7
 #include <uni10/data-structure/uni10_struct.h>
 namespace uni10{
 
